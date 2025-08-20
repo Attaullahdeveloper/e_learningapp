3364fa9b4a71faf6cfc6905c9c7f500893e78473
Attaullahdeveloper <attaullahdeveloper46@gmail.com>
2025-08-20 11:45:00 +0500

    update

---
+++ lib/Model/Controllers/constants/Appcolors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class Appcolors{
+  static Color bluemain=Color(0xff0961F5);
+  static Color backgroundcolor=Color(0xffF5F9FF);
+  static Color bluebuttoncolor=Color(0xff0961F5);
+  static Color blackcolor=Color(0xff000000);
+  static Color greenbuttoncolor=Color(0xff167F71);
+  static Color whitecolor=Color(0xffFFFFFF);
+  static Color subtextcolor=Color(0xff545454);
+  static Color maintextcolor=Color(0xff202244);
+
+}
